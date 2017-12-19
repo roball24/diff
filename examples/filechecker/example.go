0290dde1bd0eb8dd0630303f57ec511aada587ba
Robert Allen <robby.allen24@gmail.com>
2017-12-19 13:01:14 -0500

    filechecker example

---
+++ examples/filechecker/example.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/roball24/go-diff"
+)
+
+func main() {
+	equal, err := diff.DefaultFileCheck("file.txt", "equal.txt", true) // file1, file2, verbose
+	if err != nil {
+		log.Fatalf("FileCheck Error: %s\n\n", err)
+	}
+	fmt.Printf("diffcheck...%t\n", equal)
+}
